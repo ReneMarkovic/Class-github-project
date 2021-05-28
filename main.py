ad339cb50565e619e5104c7a9bedbaa6e7b968aa
Rene Markovič <41106629+ReneMarkovic@users.noreply.github.com>
2021-05-28 09:46:17 +0200

    posodobitev main funkcije

--- main.py
+++ main.py
@@ -2,9 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import networkx as nx
 from funkcije import network
+from funkcije import public_oppinion as po
 
 tip='SW'
 oblika='circle'
 N=100
 
 G, pos=network.network(tip,oblika, N)
+
+##Evolucija javnega mnenja
+#init parametri za model javnega mnenja#
+za=0.1
+proti=0.1
+nevem=1-za-proti
+G=po.opnion(G,za,nevem,proti)
+#Rezultat je mre
+
+#SEIR - #

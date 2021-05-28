f28f0f63173654000e6edce3f328dffd0cf6eb83
Rene Markovič <41106629+ReneMarkovic@users.noreply.github.com>
2021-05-28 10:49:09 +0200

    Vizualizacija

---
+++ funkcije/viz.py
@@ -0,0 +1,8 @@
+#izris mreze
+  -> Barva vozlišča odraža stanje vozlišča (stanje se navezuje na: menje, okužba, število povezav)
+  
+#Porazdelitev povezav
+  -> Barva vozlišča odraža stanje vozlišča (stanje se navezuje na: menje, okužba, število povezav)
+  
+#Časovni potek epidemije
+  -> Barva vozlišča odraža stanje vozlišča (stanje se navezuje na: menje, okužba, število povezav)

424a6325c9e13d295379f19d1d61f698d1076ac5
Blaž Švajger <83810347+LittleSvajger@users.noreply.github.com>
2021-05-29 17:41:02 +0200

    funkcija narejena

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import networkx as nx
 from funkcije import network
-from funkcije import public_oppinion as po
+#from funkcije import public_oppinion as po
 from funkcije import SEIR as seir
 
 #----------1 DEL---------------#
 tip='SW'
 oblika='circle'
-N=100
+N=1000
 
 G, pos=network.network(tip,oblika, N)
 
 
-
+'''
 #----------2 DEL---------------#
 ##Evolucija javnega mnenja
 #init parametri za model javnega mnenja#
@@ -22,8 +22,15 @@ proti=0.1
 nevem=1-za-proti
 G=po.opnion(G,za,nevem,proti)
 #Rezultat je mreza, kjer so menja vozlišč določena po principu minimalne energije
-
+'''
 #----------3 DEL---------------#
-seir(G)
+
+tmaxit=200
+beta=0.45
+sigma=1/5.1
+gamma=1/7
+initE=100
+ponovitev=10
+seir.SEIR(G,ponovitev,tmaxit,beta,sigma,gamma,initE)
 
 #----------3 DEL---------------#

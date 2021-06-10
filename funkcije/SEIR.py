b54eaf27a3a8177343ac4b0e75f3596ce2747713
Blaž Švajger <83810347+LittleSvajger@users.noreply.github.com>
2021-06-10 15:18:02 +0200

    izpis Recovered

--- funkcije/SEIR.py
+++ funkcije/SEIR.py
@@ -9,7 +9,7 @@ from seirsplus.utilities import *
 import pandas as pd
 import os
 
-'''tmaxit=200
+tmaxit=1000'''
 beta=0.45
 sigma=1/5.1
 gamma=1/7
@@ -21,7 +21,8 @@ p=0.1 # verjetnost za dajnosezno povezavi v SW mrezi
 seed=100
 G=nx.watts_strogatz_graph(N,k,p)
 tip='SW'''
-
+Nlist=[N for i in range(tmaxit+1)]
+print(Nlist)
 def SEIR(G,ponovitev,tmaxit,beta,sigma,gamma,initE,N,tip):
     G=G
     '''tmaxit=200
@@ -72,7 +73,6 @@ def SEIR(G,ponovitev,tmaxit,beta,sigma,gamma,initE,N,tip):
         dfnode.to_csv(potf+"pon=_%d_Nodes.txt"%(pon))
         
         
-        
         list0=np.arange(0,tmaxit+1,1)
         for t in range (tmaxit):    
             model.run_iteration()
@@ -97,7 +97,7 @@ def SEIR(G,ponovitev,tmaxit,beta,sigma,gamma,initE,N,tip):
         Q_E = model.numQ_E        # time series of Q_E counts
         Q_I = model.numQ_I        # time series of Q_I counts
         #print(type(S))str(t),str(S),str(E),
-        #print(F)
+        
         #results=str(t),str(model.numS),str(model.numE),str(model.numI),str(model.numR),str(model.numF),str(model.numQ_E),str(model.numQ_I)
         list1 =t.tolist()
         list2 =S.tolist()
@@ -107,19 +107,26 @@ def SEIR(G,ponovitev,tmaxit,beta,sigma,gamma,initE,N,tip):
         list6 =F.tolist()
         list7 =Q_E.tolist()
         list8 =Q_I.tolist()
-
+        Rlist=[]
+        print(len(Nlist),len(list0),len(list1),len(list2),len(list3),len(list4),len(list5))
+        for i in range(tmaxit+1):
+            difference = Nlist[i]-list2[i]-list3[i]-list4[i]-list6[i]
+            Rlist.append(difference)
+        print(Rlist)
         listnodeafter=X2.tolist()
 
         #print(list0,list1)
-        #print(len(list0),len(list1),len(list2),len(list3),len(list4),len(list5))
+        
         #plt.plot(list1)
         '''plt.plot(list2)
         plt.show()
-        plt.plot(list5)
+        plt.plot(list3)
         plt.show()
-        plt.plot(list7)
+        plt.plot(list4)
         plt.show()
-        plt.plot(list8)
+        plt.plot(list6)
+        plt.show()
+        plt.plot(Rlist)
         plt.show()'''
         #print(len(list0),len(list2))
         #print(listnode)
@@ -131,7 +138,7 @@ def SEIR(G,ponovitev,tmaxit,beta,sigma,gamma,initE,N,tip):
             "S":list2,
             "E":list3,
             "I":list4,
-            "R":list6
+            "R":Rlist
         }
         
         df=pd.DataFrame.from_dict(df)
